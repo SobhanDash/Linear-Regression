ba6a8704e5396861ceceabc679f55aa0760f3f48
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2020-08-23 15:45:51 +0530

    Update Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -2,23 +2,27 @@ import pandas as pd    #import pandas lib
 import numpy as np     #import numpy lib
 import sklearn         #import sklearn lib
 from sklearn import linear_model
+from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
 
+style.use("ggplot")
 
 data = pd.read_csv("student-mat.csv", sep= ';')           #reads the csv
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]      #slices what attributes we want to use
 
 predict = "G3"          #what we want to predict
+data = shuffle(data) # Optional - shuffle the data
 
 x = np.array(data.drop([predict], 1))      #features
 y = np.array(data[predict])                #labels
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
 #divide the data into testing and training halves
-'''best = 0
+# TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
+best = 0
 for _ in range(40):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
@@ -31,14 +35,15 @@ for _ in range(40):
     if acc > best:
         best = acc
         with open("studentmodel.pickle", "wb") as f:
-            pickle.dump(linear, f)'''
+            pickle.dump(linear, f)
 
 pickle_rick = open("studentmodel.pickle", "rb")
 linear = pickle.load(pickle_rick)
 
-
+print("-------------------------")
 print("Coeff: \n", linear.coef_)                   #for coefficient
 print("Intercepts: \n", linear.intercept_)         #for intercept
+print("-------------------------")
 
 
 predictions = linear.predict(x_test)               #for predicting
@@ -49,7 +54,6 @@ for x in range(len(predictions)):                  #how many times to run the da
 
 # Drawing and plotting model
 p = "studytime"
-style.use("ggplot")
 pyplot.scatter(data[p], data["G3"])
 pyplot.xlabel(p)
 pyplot.ylabel("Final Grade")
